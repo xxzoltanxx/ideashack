c7fc6f71919190666c821a8df02979d8d38628f1
xxzoltanxx <antonio.franjcic1@gmail.com>
2020-10-18 09:07:25 +0200

    Update Analytics.dart

--- lib/Analytics.dart
+++ lib/Analytics.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ideashack/Const.dart';
 import 'dart:ui' as ui;
 
@@ -34,156 +35,236 @@ class AnalyticsController {
     };
   }
 
-  void logInCompletedEvent() {
-    analytics.logEvent(name: 'logedIn', parameters: getBaseData());
+  void logInCompletedEvent() async {
+    try {
+      await analytics.logEvent(name: 'logedIn', parameters: getBaseData());
+    } catch (e) {
+      print(e);
+    }
   }
 
-  void consentGiven() {
-    analytics.logEvent(name: 'consentGiven', parameters: getBaseData());
+  void consentGiven() async {
+    try {
+      await analytics.logEvent(name: 'consentGiven', parameters: getBaseData());
+    } catch (e) {
+      print(e);
+    }
   }
 
-  void modalPopupShown(String type) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'type': type});
-    analytics.logEvent(name: 'modalPopupShown', parameters: parameters);
+  void modalPopupShown(String type) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'type': type});
+      await analytics.logEvent(name: 'modalPopupShown', parameters: parameters);
+    } catch (e) {}
   }
 
-  void registerModalPopupTapped() {
-    analytics.logEvent(name: 'modalPopupRegister', parameters: getBaseData());
+  void registerModalPopupTapped() async {
+    try {
+      await analytics.logEvent(
+          name: 'modalPopupRegister', parameters: getBaseData());
+    } catch (E) {}
   }
 
   void logAppLaunched() async {
-    await analytics.logAppOpen();
+    try {
+      await analytics.logAppOpen();
+    } catch (e) {}
   }
 
-  void commentTapped() {
-    analytics.logEvent(name: 'commentTapped', parameters: getBaseData());
+  void commentTapped() async {
+    try {
+      await analytics.logEvent(
+          name: 'commentTapped', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void messageTapped() {
-    analytics.logEvent(name: 'messageTapped', parameters: getBaseData());
+  void messageTapped() async {
+    try {
+      await analytics.logEvent(
+          name: 'messageTapped', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void tabSelected(String tab) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'tab': tab});
-    analytics.logEvent(name: 'tabSelected', parameters: parameters);
+  void tabSelected(String tab) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'tab': tab});
+      await analytics.logEvent(name: 'tabSelected', parameters: parameters);
+    } catch (e) {}
   }
 
-  void searchIdeaHashtagClicked(String hashtag) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'tag': hashtag});
-    analytics.logEvent(
-        name: 'searchIdeaHashtagClicked', parameters: parameters);
+  void searchIdeaHashtagClicked(String hashtag) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'tag': hashtag});
+      await analytics.logEvent(
+          name: 'searchIdeaHashtagClicked', parameters: parameters);
+    } catch (e) {}
   }
 
-  void hashTagSearched(String hashtag) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'tag': hashtag});
-    analytics.logEvent(
-        name: 'searchIdeaHashtagSearched', parameters: parameters);
+  void hashTagSearched(String hashtag) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'tag': hashtag});
+      await analytics.logEvent(
+          name: 'searchIdeaHashtagSearched', parameters: parameters);
+    } catch (e) {}
   }
 
-  void hashTagCardClicked(String hashtag) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'tag': hashtag});
-    analytics.logEvent(name: 'cardHashTagClicked', parameters: parameters);
+  void hashTagCardClicked(String hashtag) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'tag': hashtag});
+      await analytics.logEvent(
+          name: 'cardHashTagClicked', parameters: parameters);
+    } catch (e) {}
   }
 
-  void shareIdeaEntered() {
-    analytics.logEvent(name: 'shareIdeaEntered', parameters: getBaseData());
+  void shareIdeaEntered() async {
+    try {
+      await analytics.logEvent(
+          name: 'shareIdeaEntered', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void userPanelEntered() {
-    analytics.logEvent(name: 'userPanelEntered', parameters: getBaseData());
+  void userPanelEntered() async {
+    try {
+      await analytics.logEvent(
+          name: 'userPanelEntered', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void userPanelRegisterTapped() {
-    analytics.logEvent(
-        name: 'userPanelRegisterTapped', parameters: getBaseData());
+  void userPanelRegisterTapped() async {
+    try {
+      await analytics.logEvent(
+          name: 'userPanelRegisterTapped', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void browseIdeaEntered() {
-    analytics.logEvent(name: 'browseIdeasEntered', parameters: getBaseData());
+  void browseIdeaEntered() async {
+    try {
+      await analytics.logEvent(
+          name: 'browseIdeasEntered', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void userRegistered() {
-    analytics.logEvent(name: 'userRegistered', parameters: getBaseData());
+  void userRegistered() async {
+    try {
+      await analytics.logEvent(
+          name: 'userRegistered', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void upvoted(String postId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId});
-    analytics.logEvent(name: 'upvoted', parameters: parameters);
+  void upvoted(String postId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId});
+      await analytics.logEvent(name: 'upvoted', parameters: parameters);
+    } catch (e) {}
   }
 
-  void downvoted(String postId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId});
-    analytics.logEvent(name: 'downvoted', parameters: parameters);
+  void downvoted(String postId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId});
+      await analytics.logEvent(name: 'downvoted', parameters: parameters);
+    } catch (e) {}
   }
 
-  void reportTappedPost(String postId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId});
-    analytics.logEvent(name: 'reportTappedPost', parameters: parameters);
+  void reportTappedPost(String postId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId});
+      await analytics.logEvent(
+          name: 'reportTappedPost', parameters: parameters);
+    } catch (e) {}
   }
 
-  void shareClicked() {
-    analytics.logEvent(name: 'shareClicked', parameters: getBaseData());
+  void shareClicked() async {
+    try {
+      await analytics.logEvent(name: 'shareClicked', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void shareTabClicked(String type) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'type': type});
-    analytics.logEvent(name: 'shareTabClicked', parameters: parameters);
+  void shareTabClicked(String type) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'type': type});
+      await analytics.logEvent(name: 'shareTabClicked', parameters: parameters);
+    } catch (e) {}
   }
 
-  void deleteIdeaTapped(String postid) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postid});
-    analytics.logEvent(name: 'deleteIdeaTapped', parameters: parameters);
+  void deleteIdeaTapped(String postid) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postid});
+      await analytics.logEvent(
+          name: 'deleteIdeaTapped', parameters: parameters);
+    } catch (e) {}
   }
 
-  void viewCommentsTapped(String postId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId});
-    analytics.logEvent(name: 'viewCommentsTapped', parameters: parameters);
+  void viewCommentsTapped(String postId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId});
+      await analytics.logEvent(
+          name: 'viewCommentsTapped', parameters: parameters);
+    } catch (e) {}
   }
 
-  void adShown() {
-    analytics.logEvent(name: 'adShown', parameters: getBaseData());
+  void adShown() async {
+    try {
+      await analytics.logEvent(name: 'adShown', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void postedComment(String postId, String commentId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId, 'commentId': commentId});
-    analytics.logEvent(name: 'postedComment', parameters: parameters);
+  void postedComment(String postId, String commentId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId, 'commentId': commentId});
+      await analytics.logEvent(name: 'postedComment', parameters: parameters);
+    } catch (e) {}
   }
 
-  void reportTappedComment(String postId, String commentId) {
-    Map<String, dynamic> parameters = getBaseData();
-    parameters.addAll({'postId': postId, 'commentId': commentId});
-    analytics.logEvent(name: 'reportTappedComment', parameters: parameters);
+  void reportTappedComment(String postId, String commentId) async {
+    try {
+      Map<String, dynamic> parameters = getBaseData();
+      parameters.addAll({'postId': postId, 'commentId': commentId});
+      await analytics.logEvent(
+          name: 'reportTappedComment', parameters: parameters);
+    } catch (e) {}
   }
 
-  void browseEndReached() {
-    analytics.logEvent(name: 'browseEndReached', parameters: getBaseData());
+  void browseEndReached() async {
+    try {
+      await analytics.logEvent(
+          name: 'browseEndReached', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void loadingBatch() {
-    analytics.logEvent(name: 'loadingBatch', parameters: getBaseData());
+  void loadingBatch() async {
+    try {
+      await analytics.logEvent(name: 'loadingBatch', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void swiped() {
-    analytics.logEvent(name: 'swiped', parameters: getBaseData());
+  void swiped() async {
+    try {
+      await analytics.logEvent(name: 'swiped', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void dmSent() {
-    analytics.logEvent(name: 'dmSent', parameters: getBaseData());
+  void dmSent() async {
+    try {
+      await analytics.logEvent(name: 'dmSent', parameters: getBaseData());
+    } catch (e) {}
   }
 
-  void dmInitialized() {
-    analytics.logEvent(name: 'dmInitialized', parameters: getBaseData());
+  void dmInitialized() async {
+    try {
+      await analytics.logEvent(
+          name: 'dmInitialized', parameters: getBaseData());
+    } catch (e) {}
   }
 }
